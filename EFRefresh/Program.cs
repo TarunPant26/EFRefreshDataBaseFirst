ef6c3bd5a82e2715197622b1471b1ec352744821
Tarun Pant <tarunpant26@hotmail.com>
2020-12-06 12:15:25 +0530

    Add project files.

---
+++ EFRefresh/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFRefreshDBFirstApproch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DBFirstEntities();
+
+            var post = new Post()
+            {
+                Title = "FirstPost",
+                DatePublished = DateTime.Now,
+                Body = "This is Database First Blog",
+                PostID = 1
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
